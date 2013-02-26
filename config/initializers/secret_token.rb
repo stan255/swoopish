0c79d41a07b3121cc50fe3e7163d878241e767df
Stanley Marketing Jedi <stan_255@hotmail.com>
2013-02-26 23:26:27 +0000

    Started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swoopish::Application.config.secret_token = 'ef24f381989e2aa7cb7052daae6a843245f9e35c3b11fc87460017ffae0a3e433d22ce0036ac18a9912fe64adb26edae7da51f2aeded841b6d5614f7f9142fce'
